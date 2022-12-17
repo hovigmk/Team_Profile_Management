e0c14fee199027877e964dd9e34f77591ed93d94
Hovig Kalayjian <hovigkalayjian@gmail.com>
2022-12-17 18:45:17 -0500

    added the test in employee.test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,11 +3,31 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
   describe("Initialization", () => {
     it("should return the values according to the employee object", () => {
-      const employee = new Employee("Hovig", 2, "example@gmail.com");
+      const employee = new Employee("Hovig", "2", "example@gmail.com");
 
       expect(employee.name).toEqual("Hovig");
       expect(employee.id).toEqual("2");
       expect(employee.email).toEqual("example@gmail.com");
+    });
+  });
+  describe("getname", () => {
+    it("should return the name of the employee", () => {
+      expect(employee.getName()).toEqual("Hovig");
+    });
+  });
+  describe("getid", () => {
+    it("should return the id of the employee", () => {
+      expect(employee.getId()).toEqual("2");
+    });
+  });
+  describe("getemail", () => {
+    it("should return the email of the employee", () => {
+      expect(employee.getEmail()).toEqual("example@gmail.com");
+    });
+  });
+  describe("getRole", () => {
+    it("should return the role of the employee", () => {
+      expect(employee.getRole()).toEqual("employee");
     });
   });
 });

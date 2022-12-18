20aaa9c628b597506b6a642ac04a2289fa0e2025
Hovig Kalayjian <hovigkalayjian@gmail.com>
2022-12-17 19:40:20 -0500

    test passed for employee

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,10 +1,8 @@
 const Employee = require("../lib/Employee");
-
+const employee = new Employee("Hovig", "2", "example@gmail.com");
 describe("Employee", () => {
   describe("Initialization", () => {
     it("should return the values according to the employee object", () => {
-      const employee = new Employee("Hovig", "2", "example@gmail.com");
-
       expect(employee.name).toEqual("Hovig");
       expect(employee.id).toEqual("2");
       expect(employee.email).toEqual("example@gmail.com");
@@ -27,7 +25,7 @@ describe("Employee", () => {
   });
   describe("getRole", () => {
     it("should return the role of the employee", () => {
-      expect(employee.getRole()).toEqual("employee");
+      expect(employee.getRole()).toEqual("Employee");
     });
   });
 });
